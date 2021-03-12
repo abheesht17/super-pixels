96faf2e8898c82e1b8e4582853d6ead5b4732571
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-03-13 02:14:39 +0530

    Fix minor issues (#24)
    * Update Makefile
    
    * Minor Fixes

--- src/datasets/hf_image_classification.py
+++ src/datasets/hf_image_classification.py
@@ -65,10 +65,10 @@ class HFImageClassification:
 
         self.train_dataset.set_format("torch", columns=["image", "label"])
 
-        print(
-            np.mean(self.train_dataset["train"]["image"].numpy(), axis=(0, 2, 3)),
-            np.std(self.train_dataset["train"]["image"].numpy(), axis=(0, 2, 3)),
-        )
+        # print(
+        #     np.mean(self.train_dataset["train"]["image"].numpy(), axis=(0, 2, 3)),
+        #     np.std(self.train_dataset["train"]["image"].numpy(), axis=(0, 2, 3)),
+        # )
 
     def prepare_features(self, examples):
         images = []
