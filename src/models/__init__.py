2adf1255e7ef60c61cbaaec84d967ca1a7f7a26b
blackrosedragon2 <helloharshit68@gmail.com>
2021-03-21 15:17:11 +0530

    bufixes

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,4 @@
 """Package Exports for Models."""
 from src.models.simple_cnn import SimpleCnn
 from src.models.simple_gcn import SimpleGcn
-from src.models.MoNET import MoNET
+from src.models.MoNet import MoNet
