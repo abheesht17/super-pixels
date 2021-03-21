2b3b2520d875caedebcbb93d5b8198abc319fb70
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-03-21 08:36:10 +0530

    Fix merge error in viz

--- src/utils/viz.py
+++ src/utils/viz.py
@@ -8,10 +8,6 @@ def switch_coordinates(coordinates_list):
     b = coordinates_list[:, 1]
     return np.array(list(zip(b, a)))
 
-<<<<<<< HEAD
-=======
-
->>>>>>> a7657d30afbc8000abc98cc40ce33113781cbc53
 def visualize_geometric_graph(graph, file_name="Graph.png"):
     # print("Pos",graph.pos.shape)
     # print("X", graph.x.shape)
