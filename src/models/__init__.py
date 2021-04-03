8f9450e2617b3789a94842616dedf190c73f5fa6
Abheesht Sharma <sharmabhee@gmail.com>
2021-04-03 17:15:55 +0530

    Revert GAT back to ModuleList structure

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,5 @@
 """Package Exports for Models."""
 from src.models.pretrained_vgg import PretrainedVGG
 from src.models.simple_cnn import SimpleCnn
-from src.models.simple_gcn import SimpleGcn
 from src.models.simple_gat import SimpleGAT
+from src.models.simple_gcn import SimpleGcn
