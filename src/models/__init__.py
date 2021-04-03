f377bf640603ebd9a26e382f16f3605af72cf642
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-04-03 18:11:35 +0530

    Add CNN GCN Projection (#37)
    * Init CNN GCN
    
    * Add CNN+GCN Projection

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,3 +3,4 @@ from src.models.pretrained_vgg import PretrainedVGG
 from src.models.simple_cnn import SimpleCnn
 from src.models.simple_gat import SimpleGAT
 from src.models.simple_gcn import SimpleGcn
+from src.models.cnn_gcn_projection import CnnGcnProjection
