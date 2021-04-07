bf4c63f8476a8316ffcc1b435d9ec8599e84de72
blackrosedragon2 <helloharshit68@gmail.com>
2021-04-07 11:24:41 +0530

    Add Modular MoNet

--- src/models/monet.py
+++ src/models/monet.py
@@ -1,7 +1,7 @@
 """ Implementation of "Gaussian Mixture Model Convolutional Networks" (CVPR 17) """
 import math
 import torch
-from torch.nn import Parameter
+from torch.nn import (Parameter,ModuleList,Sequential,ReLU,Linear, Module)
 from torch_geometric.nn import GMMConv
 import torch.nn.functional as F
 import torch_geometric.transforms as T
@@ -15,30 +15,49 @@ def normalized_cut_2d(edge_index, pos):
     return normalized_cut(edge_index, edge_attr, num_nodes=pos.size(0))
 
 @configmapper.map("models", "monet")
-class MoNet(torch.nn.Module):
+class MoNet(Module):
     def __init__(self, config):
         super(MoNet, self).__init__()
-        self.conv1 = GMMConv(1, 32, dim=2, kernel_size=config.kernel_size)
-        self.conv2 = GMMConv(32, 64, dim=2, kernel_size=config.kernel_size)
-        self.conv3 = GMMConv(64, 64, dim=2, kernel_size=config.kernel_size)
-        self.fc1 = torch.nn.Linear(64, 128)
-        self.fc2 = torch.nn.Linear(128, 10)
+        monet_hidden_layer_sizes = [config.num_input_channels] + list(
+            config.monet_params.hidden_layer_sizes
+        )
+        linear_layer_sizes = (
+            [monet_hidden_layer_sizes[-1]]
+            + list(config.linear_layer_params.intermediate_layer_sizes)
+            + [config.num_classes]
+        )
+        self.monet_layers = ModuleList(
+            [
+                GMMConv(in_channels=in_channels, out_channels=out_channels, dim=2, kernel_size=config.kernel_size)
+                for in_channels, out_channels in zip(
+                    monet_hidden_layer_sizes[:-1], monet_hidden_layer_sizes[1:]
+                )
+            ]
+        )
+        self.linear_layers = ModuleList(
+            [
+                Linear(in_features=in_features, out_features=out_features)
+                for in_features, out_features in zip(
+                    linear_layer_sizes[:-1], linear_layer_sizes[1:]
+                )
+            ]
+        )
 
-    def forward(self, data):
-        data.x = F.elu(self.conv1(data.x, data.edge_index, data.edge_attr))
-        weight = normalized_cut_2d(data.edge_index, data.pos)
-        cluster = graclus(data.edge_index, weight, data.x.size(0))
-        data.edge_attr = None
-        data = max_pool(cluster, data, transform=T.Cartesian(cat=False))
+    def forward(self, graph):
+        data = graph
+        for i,monet_layer in enumerate(self.monet_layers[:-1]):
+            data.x = F.relu(monet_layer(data.x, data.edge_index, data.edge_attr))
+            weight = normalized_cut_2d(data.edge_index, data.pos)
+            cluster = graclus(data.edge_index, weight, data.x.size(0))
+            if i == 0:
+                data.edge_attr = None #Check what this does!!!
+            data = max_pool(cluster, data, transform=T.Cartesian(cat=False))
 
-        data.x = F.relu(self.conv2(data.x, data.edge_index, data.edge_attr))
-        weight = normalized_cut_2d(data.edge_index, data.pos)
-        cluster = graclus(data.edge_index, weight, data.x.size(0))
-        data = max_pool(cluster, data, transform=T.Cartesian(cat=False))
+        data.x =self.monet_layers[-1](data.x, data.edge_index, data.edge_attr)
 
-        data.x = F.elu(self.conv3(data.x, data.edge_index, data.edge_attr))
+        for linear_layer in self.linear_layers[:-1]:
+            x = global_mean_pool(data.x, data.batch)
+            x = F.relu(linear_layer(x))
+            x = F.dropout(x)
 
-        x = global_mean_pool(data.x, data.batch)
-        x = F.elu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        return F.log_softmax(self.fc2(x), dim=1)
+        return F.log_softmax(self.linear_layers[-1](x), dim=1)
