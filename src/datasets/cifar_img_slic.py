5fa3011bb51552e45859195b140d378e1970ce03
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-04-24 12:47:52 +0530

    Fix CIFAR 10,100

--- src/datasets/cifar_img_slic.py
+++ src/datasets/cifar_img_slic.py
@@ -1,4 +1,4 @@
-"""Implements MNIST Img Slic sDataset"""
+"""Implements CIFAR Img Slic sDataset"""
 import struct
 
 import numpy as np
@@ -9,9 +9,10 @@ from torchvision import transforms
 from src.modules.transforms import *
 from src.utils.mapper import configmapper
 import pandas as pd
+import pickle
 
-@configmapper.map("datasets", "mnist_img_slic")
-class MnistImgSlic(Dataset):
+@configmapper.map("datasets", "cifar_img_slic")
+class CifarImgSlic(Dataset):
     def __init__(self, config):
         super().__init__()
         self.config = config
@@ -44,22 +45,20 @@ class MnistImgSlic(Dataset):
             else None
         )
         
-        with open(config.filepath.image, "rb") as f:
-            # First 16 bytes contain some metadata
-            _ = f.read(4)
-            size = struct.unpack(">I", f.read(4))[0]
-            _ = f.read(8)
-            self.images = np.frombuffer(f.read(), dtype=np.uint8).reshape(size, 28, 28)
-        # Labels
-        with open(config.filepath.labels, "rb") as f:
-            # First 8 bytes contain some metadata
-            _ = f.read(8)
-            self.labels = np.frombuffer(f.read(), dtype=np.uint8)
-
+        with open(config.filepath.data,'rb') as f:
+            try:
+                self.data = pickle.load(f)
+            except:
+                self.data = pickle.load(f,encoding="bytes")
+        self.images = self.data[b'data']
+        self.labels = self.data[config.label.encode('UTF-8')]
+            
         if config.filepath.indices_csv != None:
-            filtered_indices = list(pd.read_csv(config.filepath.indices_csv)["index"])
+            filtered_indices = list(pd.read_csv(config.filepath.indices_csv)['index'])
             self.images = np.take(self.images, filtered_indices, axis=0)
             self.labels = np.take(self.labels, filtered_indices, axis=0)
+        
+        self.images = self.images.reshape(-1, 32,32,3)
 
     def __len__(self):
         return self.labels.shape[0]
