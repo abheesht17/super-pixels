a15844ffa91ca92e3531e27e90b032812209d507
blackrosedragon2 <helloharshit68@gmail.com>
2021-05-10 10:30:28 +0530

    Add OVH Requirements Script

---
+++ ovh_requirements.sh
@@ -0,0 +1,10 @@
+conda install -y pytorch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0 cudatoolkit=10.2 -c pytorch
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.8.0+cu102.html
+pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.8.0+cu102.html
+pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.8.0+cu102.html
+pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.8.0+cu102.html
+pip install torch-geometric
+pip install scikit-image==0.18.1
+pip install tensorboard==2.4.1
+pip install datasets==1.4.1
+pip install PyYAML==5.4.1
